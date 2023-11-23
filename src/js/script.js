d9c20d69e755ec47484cc09e731c4c48c3297048
Henry Webb <64293535+webbhenry1@users.noreply.github.com>
2023-11-23 10:36:10 -0500

    Basic UI setup

--- src/js/script.js
+++ src/js/script.js
@@ -7,11 +7,30 @@ window.onscroll = function() {
         menu.style.display = 'flex';
     }
 };
-// Toggle mobile menu
-function toggleMenu() {
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('active');
-}
+
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all control elements
+    var controls = document.querySelectorAll('.control');
+
+    // Add click event to each control
+    controls.forEach(function(control) {
+        control.addEventListener('click', function(e) {
+            e.preventDefault();
+            var targetId = this.getAttribute('href');
+            var targetSection = document.querySelector(targetId);
+
+            // Scroll to the target section
+            targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+            // Add active class to the clicked control and remove from others
+            controls.forEach(function(ctrl) {
+                ctrl.classList.remove('active-btn');
+            });
+            this.classList.add('active-btn');
+        });
+    });
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     // Attach click event to menu toggle button
